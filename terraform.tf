2bc0ee21cb9c844b598fbf0e6a09ae60ebef7999
Richard Russell <richard.russell@hashicorp.com>
2023-04-04 16:56:39 +0100

    Move vars and provider config into separate files

---
+++ terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aci = {
+      source  = "CiscoDevNet/aci"
+      version = "2.6.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+
+  }
+}
+
+provider "aci" {
+  # cisco-aci user name
+  username = var.apic_user
+  # cisco-aci password
+  password = var.apic_password
+  # cisco-aci url
+  url      = var.apic_url
+  insecure = true
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable apic_user {
+  type = string
+  default = "admin"
+}
+
+variable apic_password {
+  type = string
+  default = "admin"
+}
+
+variable apic_url {
+  type = string
+  default = "admin"
+}
+
+variable epg_count {
+  type = number
+  default = 10
+}
+
+variable name_prefix {
+  type = string
+  description = "prefix for object names"
+}
